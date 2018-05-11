fe8474da91a78d10abfb93a9c19225a23dbce6da
MaiThi <maihathi.92@gmail.com>
2018-05-11 20:07:21 +0200

    put file test for Jenkins

---
+++ event_ticket/tests/test.js
@@ -0,0 +1,24 @@
+var now = Date.now();
+var fs = require('fs');
+Number.prototype.padLeft = function(base,chr){
+    var  len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+}
+
+var d = new Date,
+    dformat = [ (d.getMonth()+1).padLeft(),
+            d.getDate().padLeft(),
+            d.getFullYear()].join(':')+ ' '+
+        [ d.getHours().padLeft(),
+            d.getMinutes().padLeft(),
+            d.getSeconds().padLeft()].join(':');
+var now = new Date();
+
+console.log(now.toISOString());
+fs.writeFile("result/output.txt", dformat,function (err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
